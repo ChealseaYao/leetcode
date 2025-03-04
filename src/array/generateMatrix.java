37aae50529b5ae29e0233b51eb53ee347a2569f7
ChealseaYao <successsssssss1126@outlook.com>
2025-03-04 16:54:23 +1030

    array 11

---
+++ src/array/generateMatrix.java
@@ -0,0 +1,41 @@
+package array;
+
+public class generateMatrix {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[][] generateMatrix(int n) {
+        int[][] array = new int[n][n];
+        int startX = 0;
+        int startY = 0;
+        int offSet = 1;
+        int loop = 0;
+        int count = 1;
+        int i = 0, j = 0;
+
+
+        while (loop <= (n / 2)) {
+            for (j = startY; j < n - offSet; j++) {
+                array[startX][j] = count++;
+            }
+            for (i = startX; i < n - offSet; i++) {
+                array[i][j] = count++;
+            }
+            for (; j > startY; j--) {
+                array[i][j] = count++;
+            }
+            for (; i > startX; i--) {
+                array[i][j] = count++;
+            }
+            startX++;
+            startY++;
+            offSet++;
+            loop++;
+        }
+        if (n % 2 == 1) {
+            array[i][j] = count;
+        }
+        return array;
+    }
+}
